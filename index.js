2e622e504142ae16e9259063384a0827a5544989
Ravi Nandan Ray <ravinandanray99@gmail.com>
2023-11-24 14:34:44 +0530

    Adding form data collection

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function  openTab(tabName){
 lightBoxClose = function() {
     document.querySelector(".lightbox").classList.add("closed");
 }
+let userName = document.getElementById('Name').value;
+let phone = document.getElementById('Number').value;
+let email = document.getElementById('Email').value;
+let message = document.getElementById('Message').value;
+let messageBody = "Name " + userName + "<br/> Phone " + phone + "<br/> Email " + email;
+console.log(messageBody);
+function collectData(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ravinandanray99@gmail.com",
+        Password : "2561EFA946BA99D8DDCDE91C2B135C7B6689",
+        To : 'ravinandanray99@gmail.com',
+        From : "ravinandanray99@gmail.com",
+        Subject : "This is the subject",
+        Body : messageBody
+    }).then(
+      message => {
+        if(message=='OK'){
+            swal("Secussfuly sent", "You clicked the button!", "success");
+        }
+        else{
+            swal("Error", "You clicked the button!", "error");
+        }
+      }
+    );
+}
