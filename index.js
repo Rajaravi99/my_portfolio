bc6ec07201cc1743eb39c7f4fd62853a1b247f93
Ravi Nandan Ray <ravinandanray99@gmail.com>
2023-12-02 16:42:32 +0530

    Now taking form data

--- index.js
+++ index.js
@@ -10,27 +10,24 @@ function  openTab(tabName){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
+
 lightBoxClose = function() {
     document.querySelector(".lightbox").classList.add("closed");
 }
-let userName = document.getElementById('Name').value;
-let phone = document.getElementById('Number').value;
-let email = document.getElementById('Email').value;
-let message = document.getElementById('Message').value;
-let messageBody = "Name " + userName + "<br/> Phone " + phone + "<br/> Email " + email + "<br/> Message" + message;
-console.log(userName);
-console.log(phone);
-console.log(email);
-console.log(message);
-console.log(messageBody);
+
 function collectData(){
+    let userName = document.getElementById('Name').value;
+    let phone = document.getElementById('Number').value;
+    let email = document.getElementById('Email').value;
+    let message = document.getElementById('Message').value;
+    let messageBody = "Name : " + userName + "<br/> Phone : " + phone + "<br/> Email : " + email + "<br/> Message : " + message;
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "ravinandanray99@gmail.com",
         Password : "2561EFA946BA99D8DDCDE91C2B135C7B6689",
         To : 'ravinandanray99@gmail.com',
         From : "ravinandanray99@gmail.com",
-        Subject : "This is the subject",
+        Subject : "Someone filled my portfolio contact form",
         Body : messageBody
     }).then(
       message => {
