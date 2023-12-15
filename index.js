1643eddfd93a850f8b0cf8a61b05e449769d1fdc
Ravi Nandan Ray <ravinandanray99@gmail.com>
2023-12-15 23:10:57 +0530

    added prompt box and sends email when someone opens website

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var spec = prompt("Who are you?", "");
+console.log(spec);
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "ravinandanray99@gmail.com",
+    Password : "2561EFA946BA99D8DDCDE91C2B135C7B6689",
+    To : 'ravinandanray99@gmail.com',
+    From : "ravinandanray99@gmail.com",
+    Subject : "Someone filled my portfolio contact form",
+    Body : "\""+spec+"\""+" Opened your portfolio website."
+});
+lightBoxClose = function() {
+    document.querySelector(".lightbox").classList.add("closed");
+}
 let tabLinks=document.getElementsByClassName("tab-links");
 let tabContents=document.getElementsByClassName("tab-contents");
 function  openTab(tabName){
@@ -10,11 +24,6 @@ function  openTab(tabName){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
-// popup on pageload
-lightBoxClose = function() {
-    document.querySelector(".lightbox").classList.add("closed");
-}
-// data collection when form is submitted
 function collectData(){
     let userName = document.getElementById('Name').value;
     let phone = document.getElementById('Number').value;
