6166eaf9162d371028233a4bb8dd60ca2653e87f
Ravi Nandan Ray <ravinandanray99@gmail.com>
2024-05-21 23:39:08 +0530

    first used express

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    res.setHeader('content-type','text/html');
-    fs.readFile('./index.html',(err,data)=>{
-        if(err){
-            res.end(err);
-        }
-        else{
-            res.end(data);
-        }
-    });
-});
-server.listen(3000,'localhost',()=>{
-    console.log('listening through port 3000');
-});
+// const http=require('http');
+// const fs=require('fs');
+// const server=http.createServer((req,res)=>{
+//     res.setHeader('content-type','text/html');
+//     fs.readFile('./index.html',(err,data)=>{
+//         if(err){
+//             res.end(err);
+//         }
+//         else{
+//             res.end(data);
+//         }
+//     });
+// });
+// server.listen(3000,'localhost',()=>{
+//     console.log('listening through port 3000');
+// });
