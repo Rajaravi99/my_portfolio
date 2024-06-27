dae89397c0dc3376c23a6bbcf1d1310ddf38cc8f
Ravi Nandan Ray <ravinandanray99@gmail.com>
2024-06-27 18:04:06 +0530

    created blogs

--- app.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-app.listen(3000);
-app.get('/',(req,res)=>{
-    res.sendFile('./index.html',{root:__dirname});
-});
-app.use((req,res)=>{
-    req.statusCode(404).sendFile('./views/404.html',{root:__dirname});
-});
